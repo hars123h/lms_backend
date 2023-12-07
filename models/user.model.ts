b6324a2de486160c7ed510ab5c85acef899b9dda
HARSH TRIPATHI <1940317@sliet.ac.in>
2023-12-07 10:48:31 +0530

    First

---
+++ models/user.model.ts
@@ -0,0 +1,127 @@
+require("dotenv").config();
+import mongoose, { Document, Model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  user_invite: string;
+  parent_invt: string;
+  grand_parent_invt: string;
+  great_grand_parent_invt:string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  firstLevelData: Array<{ userId: string }>;
+  secondLevelData: Array<{ userId: string }>;
+  thirdLevelData: Array<{ userId: string }>;
+
+
+  comparePassword: (password: string) => Promise<boolean>;
+  SignAccessToken: () => string;
+  SignRefreshToken: () => string;
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "please enter a valid email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false,
+    },
+    user_invite: {
+      type: String,
+      minlength: 6,
+      
+    },
+    parent_invt: {
+      type: String,
+    },
+    grand_parent_invt: {
+      type: String,
+    },
+    great_grand_parent_invt: {
+      type: String,
+    },
+
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+    firstLevelData: [{}],
+    secondLevelData: [{}],
+    thirdLevelData: [{}],
+
+  },
+  { timestamps: true }
+);
+
+// Hash Password before saving
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// sign access token
+userSchema.methods.SignAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "", {
+    expiresIn: "5m",
+  });
+};
+
+// sign refresh token
+userSchema.methods.SignRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN || "", {
+    expiresIn: "3d",
+  });
+};
+
+// compare password
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+
+export default userModel;
---
+++ server.ts
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import { app } from "./app";
+import connectDb from "./utils/db";
+require("dotenv").config();
+import http from "http";
+import { initSocketServer } from "./socketServer";
+const server = http.createServer(app);
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+});
+
+initSocketServer(server);
+// console.log("Server",server);
+
+
+//create server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is connected with port ${process.env.PORT}`);
+  connectDb();
+});
